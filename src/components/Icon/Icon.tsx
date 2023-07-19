ac97847d3bed86eca63d9813d8919eaf016fc2a7
ekb1zh <ekb1zh@gmail.com>
2023-07-19 02:26:33 +0200

    Add Icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,9 @@
+import { ReactComponent as EyeOpen } from 'src/icons/EyeOpen.svg'
+import { ReactComponent as EyeClose } from 'src/icons/EyeClose.svg'
+
+const Icon = {
+  EyeOpen,
+  EyeClose,
+}
+
+export default Icon
