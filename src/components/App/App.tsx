ffe187d72c63ab5151047ce04a59dbc6e7efd0c5
ekb1zh <ekb1zh@gmail.com>
2023-08-11 22:00:05 +0200

    Add App component base code

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Main from 'src/components/Main'
 import Input from 'src/components/Input'
@@ -6,10 +6,26 @@ import Form from 'src/components/Form'
 import Button from 'src/components/Button'
 
 const App: React.FC = () => {
+  const [loginValue, setLoginValue] = useState('')
+  const [passwordValue, setPasswordValue] = useState('')
+
   return (
     <Main>
       <Form>
-        <Input />
+        <Input
+          label='Login'
+          type='text'
+          value={loginValue}
+          onChange={setLoginValue}
+        />
+
+        <Input
+          label='Password'
+          type='password'
+          value={passwordValue}
+          onChange={setPasswordValue}
+        />
+
         <Button>Sing In</Button>
       </Form>
     </Main>
