f8ab2766fd229bcbc94a63ef71ceecbe6a2c3e07
ekb1zh <ekb1zh@gmail.com>
2023-08-11 17:28:29 +0200

    Update input logic

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,28 +1,101 @@
-import React, { useMemo } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 
 import { StringGenerator } from 'src/utils'
 import styles from 'src/components/Input/Input.module.scss'
+import * as T from 'src/components/Input/types'
 
 const stringGenerator = new StringGenerator({
   allowedChars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
   defaultSize: 4,
 })
 
-const Input: React.FC = () => {
-  const inputId = useMemo(() => stringGenerator.next(), [])
+const Input: React.FC<T.InputProps> = ({ type = 'password' }) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const [isValueVisible, setIsValueVisible] = useState(type !== 'password')
+  const [value, setValue] = useState('')
+  const [isFocused, setIsFocused] = useState(false)
+  const [classes, setClasses] = useState(styles)
+
+  const id = useMemo(() => stringGenerator.next(), [])
+
+  const onChangeInput: React.InputHTMLAttributes<HTMLInputElement>['onChange'] =
+    ({ target: { value } }) => setValue(value)
+
+  const onFocusInput = () => setIsFocused(true)
+  const onBlurInput = () => setIsFocused(false)
+
+  const onMouseDownEyeButton: React.ButtonHTMLAttributes<HTMLButtonElement>['onClick'] =
+    (event) => {
+      /*
+        Prevent focus out
+      */
+      if (document.activeElement === inputRef.current) {
+        event.preventDefault()
+      }
+
+      /*
+        Update isValueVisible
+      */
+      setIsValueVisible((prev) => !prev)
+    }
+
+  useEffect(() => {
+    setIsValueVisible(type !== 'password')
+  }, [type])
+
+  useEffect(() => {
+    const classes = { ...styles }
+
+    /*
+      Update label styles
+    */
+    {
+      const isValueEmpty = value.length === 0
+      let label = classes.Label
+
+      label += ' '
+      label += isFocused
+        ? classes.Label_small
+        : isValueEmpty
+        ? classes.Label_big
+        : classes.Label_small
+
+      classes.Label = label
+    }
+
+    setClasses(classes)
+  }, [isFocused, value.length])
 
   return (
-    <div className={styles.RootContainer}>
-      <div className={styles.InnerContainer}>
-        <input className={styles.Input} id={inputId} />
-        <label className={styles.Label} htmlFor={inputId}>
-          label
+    <div className={classes.RootContainer}>
+      <div className={classes.InnerContainer}>
+        <label className={classes.Label} htmlFor={id}>
+          Label
         </label>
-        <button type='button' className={styles.EyeButton}>
-          show pass
-        </button>
+
+        <input
+          ref={inputRef}
+          type={isValueVisible ? 'text' : 'password'}
+          id={id}
+          className={classes.Input}
+          onChange={onChangeInput}
+          onFocus={onFocusInput}
+          onBlur={onBlurInput}
+        />
+
+        {type === 'password' && (
+          <button
+            type='button'
+            className={classes.EyeButton}
+            onMouseDown={onMouseDownEyeButton}
+          >
+            show pass
+          </button>
+        )}
       </div>
-      <span className={styles.ErrorMessage}>Error text</span>
+
+      <span className={classes.ErrorMessage}>Error text</span>
     </div>
   )
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,1 +1,2 @@
 export { default } from 'src/components/Input/Input'
+export * from 'src/components/Input/types'
