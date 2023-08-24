c2d46533a07eb92ec1a779edfd6529b271c797fa
ekb1zh <ekb1zh@gmail.com>
2023-08-25 01:40:29 +0200

    Fix

--- src/_mocks/fetch/fetch.ts
+++ src/_mocks/fetch/fetch.ts
@@ -2,7 +2,9 @@ import { DELAY_BEFORE_RESPONSE } from 'src/_mocks/fetch/constants'
 import { router } from 'src/_mocks/fetch/router'
 
 export const fetch: Window['fetch'] = async (url, options) => {
-  await new Promise((resolve) => setTimeout(resolve, DELAY_BEFORE_RESPONSE))
+  await new Promise((resolve) =>
+    window.setTimeout(resolve, DELAY_BEFORE_RESPONSE),
+  )
 
   const responseBody = router(url, options)
 
--- src/settings/applyMocks.ts
+++ src/settings/applyMocks.ts
@@ -1,7 +1,5 @@
 import { fetch } from 'src/_mocks'
 
 export const applyMocks = () => {
-  if (process.env.NODE_ENV !== 'test') {
-    window.fetch = fetch
-  }
+  window.fetch = fetch
 }
